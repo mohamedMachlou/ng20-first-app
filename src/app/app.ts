6bfe9d14758dcab3b0fb6e01337e942190e60067
Mohamed Machlou <medmachlou@gmail.com>
2025-08-09 09:42:12 +0100

    customize notification toast

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Todos } from './pages/todos/todos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Todos
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/todos/todos.ts
@@ -0,0 +1,157 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-todos',
+  imports: [FormsModule],
+  templateUrl: './todos.html',
+  styleUrl: './todos.css',
+})
+export class Todos {
+  title = 'Todos List';
+  edit = false;
+  defaultTodo = 'write a todo...';
+  showForm: boolean = false;
+  editible: boolean = false;
+  notify = signal<boolean>(false);
+  todoIndex: number = -1;
+  mytodo: string = '';
+
+  // Notification Personaliée
+  notification = {
+    message: '',
+    position: '',
+    alertClass: '',
+    duration: 3000
+
+  }
+
+  // List Todos
+  todos: string[] = ['Angular', 'Java', 'JS', 'Python', 'Django'];
+
+  // Add Todo
+  addTodo() {
+    if (this.mytodo != '') {
+      this.todos = [this.mytodo, ...this.todos];
+    }
+    this.initForm();
+    this.triggerNotify({
+      message: 'Todo created succefuly ',
+      position: 'toast toast-bottom toast-end',
+      alertClass: 'alert alert-success',
+      duration: 3000
+    });
+  }
+
+  // Edit Todo
+  editTodo(todo: string, index: number) {
+    this.mytodo = todo;
+    this.showForm = true;
+    this.editible = true;
+    this.todoIndex = index;
+
+    console.log('todo : ', todo);
+    console.log('myTodo : ', this.mytodo);
+    console.log('Editible : ', this.editible);
+  }
+
+  // Change show form status : show/hide form
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  // switch above button content: show form / hide form
+  changeToggleBtn() {
+    return this.showForm ? 'Hide Form' : 'Show Form';
+  }
+
+  // Hide and Init Form
+  initForm() {
+    this.mytodo = '';
+    this.editible = false;
+    this.todoIndex = -1;
+    this.toggleForm();
+  }
+
+  // Update Todo
+  updateTodo() {
+    this.triggerNotify({
+      message: 'Todo updated succefuly ',
+      position: 'toast toast-bottom toast-end',
+      alertClass: 'alert alert-warning',
+      duration: 3000
+    });
+    this.initForm();
+    this.todos[this.todoIndex] = this.mytodo;
+  }
+
+  // Delete Todo
+  deleteTodo(index: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Deleted!',
+          text: 'Your file has been deleted.',
+          icon: 'success',
+          timer: 2000,
+          timerProgressBar: true,
+        });
+        this.triggerNotify({
+      message: 'Todo was deleted ',
+      position: 'toast toast-bottom toast-end',
+      alertClass: 'alert alert-error',
+      duration: 3000
+    });
+        this.todos = this.todos.filter((todo, myindex) => {
+          return myindex != index;
+        });
+      }
+    });
+
+    // if (confirm('Do you want to delete this todo ?')) {
+    //    this.todos = this.todos.filter((todo, myindex) => {
+    //     return myindex != index;
+    //   });
+    // }
+  }
+
+  // Notifications
+  triggerNotify(customNotify: any) {
+
+    this.notification = {
+      ...customNotify,
+    }
+
+    this.notify.set(true);
+    console.log('Notify befor : ', this.notify)
+
+    setTimeout(() => {
+      this.notify.set(false);
+      console.log('Notify after : ', this.notify)
+    }, 3000);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
